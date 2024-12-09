23c583547b613d68a78ad7cea3c017c75e213480
Tege1337 <toth-nador.tege@diak.szbi-pg.hu>
2024-12-09 08:42:42 +0100

    Co-authored-by: Molnár Maxim <molnar.maxim@diak.szbi-pg.hu>

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+def otos_sorsolas():
+    szamok = []
+    for x in range(0, 6):
+        szamok.append(random.randint(1, 90))
+    return szamok
+
+def main(otos_sorsolas):
+    print("Szerencsejáték Zrt. 🍀")
+    print("Kérlek válaszd ki milyen típusú lottót szeretnél játszani!\n5-ös lottó\n6.-os lottó\nKiválasztani úgy tudod, hogy az adott lottó indexét beírod (1,2)")
+    valasztott_lotto = input("Választás: ")
+    if valasztott_lotto ==
+
+print()        :1""
+
+
+def handle_otos():
+    sorszam = 1
+    szamaid = []
+    for x in range(sorszam):
+        szamaid.append = input(print(f"Mi az {sorszam}. számod?"))
